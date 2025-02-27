2967619e32b0415b7257f6e791735a9479dab0fc
kach2025 <acampos1905@gmail.com>
2025-02-26 20:51:28 -0600

    primer commit

---
+++ Asp1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EjercicioAsp3_3035699
+{
+    public partial class Asp1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Response.Redirect
+                (
+                "Asp2.aspx" +
+                "?ddlCategory=" + ddlCategory.SelectedValue +
+                "&ddlSupplier=" + ddlSupplier.SelectedValue +
+                "&strProduct=" + strProduct.Text +
+                "&strDescription=" + strDescription.Text +
+                "&strImage=" + strImage.Text +
+                "&decPrice=" + decPrice.Text +
+                "&bytNumberInStock=" + bytNumberInStock.Text +
+                "&bytNumberOnOrder=" + bytNumberOnOrder.Text +
+                "&bytReorderLevel=" + bytReorderLevel.Text);
+        }
+    }
+}
