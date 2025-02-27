4df2d577205e39b5bbb7682905926bc1708ac086
kach2025 <acampos1905@gmail.com>
2025-02-26 20:56:25 -0600

    segundo commit

--- Asp2.aspx.cs
+++ Asp2.aspx.cs
@@ -11,7 +11,24 @@ namespace EjercicioAsp3_3035699
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
+            ddlCategory.SelectedValue = Request.QueryString["ddlCategory"];
+            ddlSupplier.SelectedValue = Request.QueryString["ddlSupplier"];
+            strProduct.Text = Request.QueryString["strProduct"];
+            strDescription.Text = Request.QueryString["strDescription"];
+            strImage.Text = Request.QueryString["strImage"];
+            decimal DecPrice = Convert.ToDecimal(Request.QueryString["decPrice"]);
+            decPrice.Text = DecPrice.ToString("c");
+            lblNumberInStrock.Text = Request.QueryString["bytNumberInStock"];
+            lblNumberOnOrder.Text = Request.QueryString["bytNumberOnOrder"];
+            bytReorderLevel.Text = Request.QueryString["bytReorderLevel"];
+
+            Byte byNumberInStock = Convert.ToByte(Request.QueryString["bytNumberInStock"]);
+            Byte byNumberOnOrder = Convert.ToByte(Request.QueryString["bytNumberOnOrder"]);
+            decimal decValueInStock = DecPrice * byNumberInStock;
+            decimal decValueOnOrder = DecPrice * byNumberOnOrder;
+            bytNumberInStock.Text = " (Value: " + decValueInStock.ToString("c") + ")";
+            bytNumberOnOrder.Text = " (Value: " + decValueOnOrder.ToString("c") + ")";
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
